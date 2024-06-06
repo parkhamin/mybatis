740c5aecaa408891d49d042c1d36499b86542f51
박하민 <parkhamint@naver.com>
2024-06-06 22:23:41 +0900

    게시물 작성화면 만들기

--- src/main/java/com/project/board/controller/BoardController.java
+++ src/main/java/com/project/board/controller/BoardController.java
@@ -3,9 +3,15 @@ package com.project.board.controller;
 import com.project.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save(){
+        return "save";
+    }
 }
