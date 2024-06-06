1684baebc67cc98a4d9048e6af902881e64b6da7
박하민 <parkhamint@naver.com>
2024-06-06 23:09:12 +0900

    mybatis 세팅하기

--- src/main/java/com/project/board/controller/BoardController.java
+++ src/main/java/com/project/board/controller/BoardController.java
@@ -1,9 +1,12 @@
 package com.project.board.controller;
 
+import com.project.board.dto.BoardDTO;
 import com.project.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,5 +16,11 @@ public class BoardController {
     @GetMapping("/save")
     public String save(){
         return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO){
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
     }
 }
