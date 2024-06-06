dd4124804108093af82480b6572e069578a69ca8
박하민 <parkhamint@naver.com>
2024-06-07 00:16:07 +0900

    게시글 삭제 기능 구현하기

--- src/main/java/com/project/board/controller/BoardController.java
+++ src/main/java/com/project/board/controller/BoardController.java
@@ -62,4 +62,11 @@ public class BoardController {
         model.addAttribute("board", dto);
         return "detail";
     }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        boardService.delete(id);
+        return "redirect:/list";
+    }
+
 }
