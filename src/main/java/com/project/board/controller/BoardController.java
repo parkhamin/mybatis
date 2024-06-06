ec981b939f55ff1e93a57eddb824fc1e540eb367
박하민 <parkhamint@naver.com>
2024-06-06 23:32:39 +0900

    게시글 목록 출력하기

--- src/main/java/com/project/board/controller/BoardController.java
+++ src/main/java/com/project/board/controller/BoardController.java
@@ -4,9 +4,12 @@ import com.project.board.dto.BoardDTO;
 import com.project.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,4 +27,14 @@ public class BoardController {
         boardService.save(boardDTO);
         return "index";
     }
+
+    @GetMapping("/list")
+    public String findAll(Model model){
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        System.out.println("boardDTOList = " + boardDTOList);
+        return "list";
+    }
+
+
 }
